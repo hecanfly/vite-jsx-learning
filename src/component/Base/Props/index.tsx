951bf14d8e63d73482225a09373df1e1147af80e
hecanfly <2954552801@qq.com>
2024-03-05 17:03:44 +0800

    props

--- src/component/Base/Props/index.tsx
+++ src/component/Base/Props/index.tsx
@@ -1,8 +1,44 @@
-import { defineComponent } from "vue";
+import { PropType, defineComponent, toRaw } from "vue";
 
-export default defineComponent({
-  name: "HelloTSX",
-  setup(props, ctx) {
-    return () => <h1>Hello1111 TSX</h1>;
+type Color = "red" | "blue";
+
+const MyComponent = defineComponent({
+  setup() {
+    const SonComponent = defineComponent({
+      props: {
+        isVisible: {
+          type: Boolean,
+          default: true,
+          required: true,
+        },
+        color: {
+          type: String as PropType<Color>,
+        },
+        data: {
+          type: Object,
+          default: function () {
+            return { message: "hello" };
+          },
+        },
+      },
+      setup(props) {
+        console.log(toRaw(props));
+        
+        return () => {
+          return (
+            <>
+            <h1>子组件</h1>
+              <p>isVisible：{props.isVisible === true ? "true" : "false"}</p>
+              <p>color：{props.color}</p>
+              <p>data：{props.data?.message}</p>
+            </>
+          );
+        };
+      },
+    });
+    
+    return () => <><h1>父组件</h1><hr/><SonComponent isVisible={true} color={"red"}></SonComponent></>;
   },
 });
+
+export default MyComponent;
